d8569471f26f27306bf678b991c5e923742dbf61
Alexandr <tribisalexandr@yandex.ru>
2022-12-09 00:12:19 +0300

    feat: add interfaces

---
+++ news-api/src/types/index.ts
@@ -0,0 +1,20 @@
+export interface sources {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+export interface news {
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
