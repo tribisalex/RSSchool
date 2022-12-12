f834b8540a455a350d79e4a3c6501a78a0456817
Alexandr <tribisalexandr@yandex.ru>
2022-12-13 01:58:45 +0300

    feat: change link API

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: '2b0c2736a87843beaab7e1c55c804742',
         });
     }
