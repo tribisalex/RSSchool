8bf9d97d82ade9355493ce85f4163e6b7b6f1200
Alexandr <tribisalexandr@yandex.ru>
2023-01-20 12:22:53 +0300

    feat: add page layout

---
+++ async-race/webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+//const htmlPageNames = ['main-page/main.html', 'page-404/404.html', 'cart-page/cart.html', 'product-page/product.html'];
+//
+// const multipleHtmlPlugins = htmlPageNames.map(
+//   (name) =>
+//     new HtmlWebpackPlugin({
+//       template: `./src/components/view/${name}`,
+//       filename: name.split('/')[1],
+//     })
+// );
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|ogg|mp3|wav|mp4|mpe?g)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'assets',
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new EslingPlugin({ extensions: 'ts' }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    compress: true,
+    hot: true,
+    port: 8080,
+    publicPath: '/',
+    historyApiFallback: true,
+  },
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
